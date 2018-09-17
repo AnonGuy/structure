de388118a4c64e84beaf0797a54a0a671a39fcb0
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-09-17 15:44:59 +0100

    Haaaaaands

---
+++ server/structure_server/dashboard/scraper.py
@@ -0,0 +1,15 @@
+"""Define various webscraping methods."""
+
+import requests
+
+from structure_server.dashboard.models import User
+
+
+endpoint = 'https://my.loreto.ac.uk/'
+
+
+def valid_user(user: User) -> bool:
+    """Take a User object and validate credentials."""
+    return requests.get(
+        endpoint, auth=(user.username, user.password)
+    ).status_code == 200
