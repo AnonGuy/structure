a41dd3c9cf22b0fd9a7e3b18eb0e09b2da2af75e
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-10-10 10:02:33 +0100

    Updated ALLOWED_HOSTS to permit Heroku app

--- structure_server/structure_server/settings.py
+++ structure_server/structure_server/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'structure-server.herokuapp.com'
+]
 
 
 # Application definition
